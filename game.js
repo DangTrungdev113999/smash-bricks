60d93c3e53fa4b0488f58bbf9acbe88bb18d62df
dangtrungdev <dangtrungdev113999@gmail.com>
2020-07-27 21:55:25 +0700

    rect, strokeStyle, fillStyle, fill method

--- game.js
+++ game.js
@@ -2,16 +2,10 @@ const canvas = document.getElementById("game");
 
 const context = canvas.getContext("2d");
 
-context.moveTo(0, 0);
-context.lineTo(100, 0);
-context.lineTo(100, 50);
-context.lineTo(0, 50);
-context.lineTo(0, 0);
-context.stroke();
+context.strokeStyle = "blue";
 
-context.moveTo(100, 100);
-context.lineTo(200, 100);
-context.lineTo(200, 200);
-context.lineTo(100, 200);
-context.lineTo(100, 100);
+context.rect(0, 0, 100, 50);
 context.stroke();
+context.fillStyle = "tomato";
+
+context.fill();
