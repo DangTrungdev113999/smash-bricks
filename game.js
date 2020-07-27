ceab3d15eda384a947c1b34f898c3469695d251e
dangtrungdev <dangtrungdev113999@gmail.com>
2020-07-27 22:29:58 +0700

    beginPath, closePath method

--- game.js
+++ game.js
@@ -2,10 +2,21 @@ const canvas = document.getElementById("game");
 
 const context = canvas.getContext("2d");
 
-context.strokeStyle = "blue";
-
-context.rect(0, 0, 100, 50);
+context.beginPath();
+context.rect(0, 0, 100, 100);
+context.closePath();
+context.strokeStyle = "red";
 context.stroke();
-context.fillStyle = "tomato";
+context.beginPath();
 
+context.moveTo(250, 200);
+context.arc(200, 200, 50, 0, Math.PI * 2, false);
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.moveTo(350, 300);
+context.arc(300, 300, 50, 0, Math.PI * 2, false);
 context.fill();
+context.stroke();
+context.closePath();
